9725ec28ba13988b2f9e566e127f858c6c404b2a
Pratizan2412 <iirybalko@gmail.com>
2016-04-27 19:12:26 +0300

    updated version

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,15 +1,15 @@
 public class Factorial {
     public static void main (String[] args) {
         int a = Integer.parseInt(args[0]);
-        int b = FactA ( a );
+        int b = factorialA ( a );
         if (b != 1) {
             System.out.println("FactorialA :" + b);
         }
     }
-    public static int FactA(int x) {
+    public static int factorialA(int x) {
         int fact = 1;
-        if ( x == 0 || x < 0) {
-            System.out.println("Nevernoe znachenie");
+        if ( x < 0) {
+            throw new IllegalArgumentException("Nevernoe znachenie");
         }   else {
             for (int i = 1; i <= x ; i++){
                 fact *= i;
--- test/FactorialTest.java
+++ test/FactorialTest.java
@@ -7,14 +7,19 @@ public class FactorialTest {
 
     @Test
     public void FactorialTest2(){
-        Assert.assertEquals( 2,Factorial.FactA(2));
+        Assert.assertEquals( 2,Factorial.factorialA(2));
     }
     @Test
     public void FactorialTest0(){
-        Assert.assertEquals(1, Factorial.FactA(0));
+        Assert.assertEquals(1, Factorial.factorialA(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void FactorialTestm1(){
+        Assert.assertEquals(1, Factorial.factorialA(-1));
     }
     @Test
     public void FactorialTest5(){
-        Assert.assertEquals( 120,Factorial.FactA(5));
+        Assert.assertEquals( 120,Factorial.factorialA(5));
     }
 }
